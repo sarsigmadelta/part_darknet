a7f29b5e37238c085540aa6a55b83b2f7934a3ad
vickydaydayup <vickyfq@foxmail.com>
2020-09-19 15:57:58 +0800

    col2im kernel seems ok..

--- src/col2im_gpu.cu
+++ src/col2im_gpu.cu
@@ -6,8 +6,25 @@ const int blockSize = 512;
 __global__ void col2im_gpu_kernel(float *data_col, int channels, int height, int width,
                             int ksize, int stride, int pad, 
                             int height_col, int width_col, float *im){
-    int idx = blockDim.x * blockIdx.x + threadIdx.x;
-    int 
+    int index = blockDim.x * blockIdx.x + threadIdx.x;
+    int w_im = index % width;
+    int h_im = index / width % height;
+    int c_im = index / width / height;
+    
+    int w_col_start = ( w_im < ksize) ? 0 : (w_im - ksize) / stride + 1;
+    int w_col_end = min(w_im / stride + 1, width_col);
+    int h_col_start = (h_im < ksize ) ? 0 : (h_im - ksize) / stride + 1;
+    int h_col_end = min(h_im / stride + 1, height_col);
+
+    int h_col, w_col;
+    for(h_col=h_col_start; h_col<h_col_end; ++h_col){
+        for(w_col=w_col_start; w_col<w_col_end; ++w_col){
+            int w_k = h_im - h_col * stride;
+            int h_k = w_im - w_col * stride;
+            int index_col = (c_im * ksize * ksize + h_k * ksize + w_k) * height_col * width_col + h_col * width_col + w_col;
+            im[index] += data_col[index_col];
+        }
+    }
 }
 
 
